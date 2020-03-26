0db3429cba489d9f3255ac8bb1d0b69428b16c24
Vijayraj <vijayraj.nathe@weboniselab.com>
2020-03-26 15:53:19 +0530

    added test cases for Search, Checkout, AddToCart, WatchVideo events

--- integrations/pinterest-tag/test/index.test.js
+++ integrations/pinterest-tag/test/index.test.js
@@ -114,6 +114,116 @@ describe('Pinterest', function() {
           ]
         });
       });
+
+      it('should track custom event mapped under pinterestEventMapping mapping', function() {
+        analytics.track('User Signed Up');
+        analytics.called(window.pintrk, 'track', 'Signup');
+      });
+
+      it('should track product searched', function() {
+        analytics.track('Products Searched', { query: 'product1' });
+        analytics.called(window.pintrk, 'track', 'Search', {
+          search_query: 'product1'
+        });
+      });
+
+      it('should track product list filtered', function() {
+        analytics.track('Product List Filtered', {
+          category: 'cat 1'
+        });
+        analytics.called(window.pintrk, 'track', 'Search', {
+          line_items: [
+            {
+              product_category: 'cat 1'
+            }
+          ]
+        });
+      });
+
+      it('should track product added', function() {
+        analytics.track('Product Added', {
+          product_id: '507f1f77bcf86cd799439011',
+          currency: 'USD',
+          quantity: 1,
+          price: 44.33,
+          name: 'my product',
+          category: 'cat 1',
+          sku: 'p-298',
+          value: 24.75
+        });
+
+        analytics.called(window.pintrk, 'track', 'AddToCart', {
+          value: 24.75,
+          currency: 'USD',
+          line_items: [
+            {
+              product_name: 'my product',
+              product_id: 'p-298',
+              product_category: 'cat 1',
+              product_price: 44.33,
+              product_quantity: 1
+            }
+          ]
+        });
+      });
+
+      it('should tracks order completed', function() {
+        analytics.track('Order Completed', {
+          order_id: '50314b8e9bcf000000000000',
+          total: 30,
+          revenue: 25,
+          shipping: 3,
+          tax: 2,
+          discount: 2.5,
+          coupon: 'hasbros',
+          currency: 'USD',
+          products: [
+            {
+              product_id: '507f1f77bcf86cd799439011',
+              sku: '45790-32',
+              name: 'Monopoly: 3rd Edition',
+              price: 19,
+              quantity: 1,
+              category: 'Games'
+            },
+            {
+              product_id: '505bd76785ebb509fc183733',
+              sku: '46493-32',
+              name: 'Uno Card Game',
+              price: 3,
+              quantity: 2,
+              category: 'Games'
+            }
+          ]
+        });
+
+        analytics.called(window.pintrk, 'track', 'Checkout', {
+          order_id: '50314b8e9bcf000000000000',
+          coupon: 'hasbros',
+          currency: 'USD',
+          line_items: [
+            {
+              product_name: 'Monopoly: 3rd Edition',
+              product_id: '45790-32',
+              product_category: 'Games',
+              product_price: 19,
+              product_quantity: 1
+            },
+            {
+              product_name: 'Uno Card Game',
+              product_id: '46493-32',
+              product_category: 'Games',
+              product_price: 3,
+              product_quantity: 2
+            }
+          ]
+        });
+      });
+
+      it('should track video playback started', function() {
+        analytics.track('Video Playback Started');
+        analytics.called(window.pintrk, 'track', 'WatchVideo');
+      });
     });
 
     describe('#page', function() {
